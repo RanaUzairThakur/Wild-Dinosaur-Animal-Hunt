c4b20ee198446690e4c280e2a2a15d14dd121807
RanaUzairThakur <66404667+RanaUzairThakur@users.noreply.github.com>
2022-01-04 21:01:22 -0800

    push 3

---
+++ Assets/ChaptersMoveAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaptersMoveAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        LeanTween.move(this.gameObject.GetComponent<RectTransform>(), new Vector3(0f, 0f, 0f), 0.5f).setEaseSpring().setIgnoreTimeScale(true);
+    }
+
+  
+    void OnDisable()
+    {
+        LeanTween.move(this.gameObject.GetComponent<RectTransform>(), new Vector3(3000f, 0f, 0f), 0.5f).setEaseSpring().setIgnoreTimeScale(true);
+    }
+}
---
+++ Assets/HyperCarAssets/Options.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Options : MonoBehaviour {
+
+	public void GoToTiltScene()
+	{
+		SceneManager.LoadScene ("tilt");
+	}
+	public void GoToButtonScene()
+	{
+		SceneManager.LoadScene ("button");
+	}
+	public void GoToMenuScene()
+	{
+		SceneManager.LoadScene ("Menu");
+	}
+		
+}
